ff634f9dad032ce6dfa93ed7063e2916d5a49899
stevengalvis <steven.galvis@gmail.com>
2019-03-01 22:39:46 -0500

    fixed sending bug

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const Delimiter = require("@serialport/parser-delimiter");
 
 // Open the port
 var port = new SerialPort("/dev/tty.usbserial-AL02K9OD", {
-  baudRate: 9600
+  baudRate: 115200
 });
-
-const parser = port.pipe(new Delimiter({ delimiter: "\n" }));
-
-parser.on("data", console.log);
 
 // Read the port data
 port.on("open", function() {
   console.log("open");
 
+  port.write("S");
+  port.write("I");
+  port.write("\r");
+  port.write("\n");
+
   port.on("data", function(data) {
     console.log("here", data, data.toString("utf8"));
-    // parser.on("data", line => console.log(`> ${line}`));
   });
 });
