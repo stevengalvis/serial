ec68eedf3f5ac0a7852a864952b6346ad1cc0817
stevengalvis <steven.galvis@gmail.com>
2019-03-05 22:51:11 -0500

    getting data based on time interval

--- index.js
+++ index.js
@@ -5,25 +5,68 @@ const Delimiter = require("@serialport/parser-delimiter");
 
 // Open the port
 var port = new SerialPort("/dev/tty.usbserial-AL02K9OD", {
-  baudRate: 115200
+  baudRate: 115200,
+  parser: new SerialPort.parsers.Readline("\n")
 });
 
-// Read the port data
+// // Read the port data
+// port.on("open", function() {
+//   console.log("open");
+//
+//   // port.write("S");
+//   // port.write("I");
+//   setInterval(read, 1000);
+//
+//   function read() {
+//     port.write("S");
+//     port.write("x");
+//     port.write("1");
+//     port.write("\r");
+//     port.write("\n");
+//     port.on("data", function(data) {
+//       console.log(`here`, data, data.toString("utf8"));
+//     });
+//   }
+// });
+//Read the port data
 port.on("open", function() {
   console.log("open");
 
-  // port.write("S");
-  // port.write("I");
-  setInterval(read, 1000);
+  // setInterval(readInterval, 1000);
+  let x = 0;
 
-  function read() {
-    port.write("S");
-    port.write("x");
-    port.write("1");
-    port.write("\r");
-    port.write("\n");
-    port.on("data", function(data) {
-      console.log(`here `, data, data.toString("utf8"));
-    });
+  for (let i = 1; i > x; i++) {
+    getStream();
   }
 });
+let readTicks = 0;
+
+function readInterval() {
+  port.write("S");
+  port.write("x");
+  port.write("1");
+  port.write("\r");
+  port.write("\n");
+
+  port.on("readable", function() {
+    console.log(`Seconds ${readTicks} Data:`, port.read().toString("utf8"));
+  });
+
+  readTicks++;
+}
+
+function getStream() {
+  port.write("S");
+  port.write("x");
+  port.write("1");
+  port.write("\r");
+  port.write("\n");
+
+  port.on("data", function(data) {
+    let dt = new Date();
+
+    let line = `${dt.toUTCString()} ${data.toString("utf8")}`;
+    // let line = `data ticks ${tick} ${Date.now()} ${data.toString("utf8")}`;
+    console.log(line);
+  });
+}
