5babb4f6ac1c7c73c52fe6d138fc33b7ba969a01
stevengalvis <steven.galvis@gmail.com>
2019-03-06 21:57:54 -0500

    refactored code for getting data from serial

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Require the serialport node module
 const SerialPort = require("serialport");
-const Readline = SerialPort.parsers.Readline;
-const Delimiter = require("@serialport/parser-delimiter");
 
 // Open the port
 var port = new SerialPort("/dev/tty.usbserial-AL02K9OD", {
@@ -9,35 +7,8 @@ var port = new SerialPort("/dev/tty.usbserial-AL02K9OD", {
   parser: new SerialPort.parsers.Readline("\n")
 });
 
-// // Read the port data
-// port.on("open", function() {
-//   console.log("open");
-//
-//   // port.write("S");
-//   // port.write("I");
-//   setInterval(read, 1000);
-//
-//   function read() {
-//     port.write("S");
-//     port.write("x");
-//     port.write("1");
-//     port.write("\r");
-//     port.write("\n");
-//     port.on("data", function(data) {
-//       console.log(`here`, data, data.toString("utf8"));
-//     });
-//   }
-// });
-//Read the port data
 port.on("open", function() {
   console.log("open");
-
-  // setInterval(readInterval, 1000);
-  let x = 0;
-
-  for (let i = 1; i > x; i++) {
-    getStream();
-  }
 });
 let readTicks = 0;
 
@@ -54,6 +25,8 @@ function readInterval() {
 
   readTicks++;
 }
+setInterval(readInterval, 1000);
+setInterval(getStream);
 
 function getStream() {
   port.write("S");
@@ -65,8 +38,6 @@ function getStream() {
   port.on("data", function(data) {
     let dt = new Date();
 
-    let line = `${dt.toUTCString()} ${data.toString("utf8")}`;
-    // let line = `data ticks ${tick} ${Date.now()} ${data.toString("utf8")}`;
-    console.log(line);
+    console.log(`${dt.toUTCString()} ${data.toString("utf8")}`);
   });
 }
